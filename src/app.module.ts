af6756633b158f2feb662a6aa37b792bce5cc874
JhonMartinez012 <jhonmartinez012@hotmail.com>
2022-06-08 15:37:21 -0500

    cracion de los modulos, controladores y servicios necesarios para el backend del proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StadiumModule } from './stadium/stadium.module';
+import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
+import { ConfigurationModule } from './configuration/configuration.module';
 
 @Module({
-  imports: [],
+  imports: [StadiumModule, AuthModule, AdminModule, ConfigurationModule],
   controllers: [AppController],
   providers: [AppService],
 })
