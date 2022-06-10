2db5424eb758e82c8255b0fbd0d6e496f469e610
JhonMartinez012 <jhonmartinez012@hotmail.com>
2022-06-10 17:05:58 -0500

    Intento de autenticacion

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Request, Post, UseGuards, Body } from '@nestjs/common';
+// import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Body() req){
+    return req.user;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { StadiumModule } from './stadium/stadium.module';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
 import { ConfigurationModule } from './configuration/configuration.module';
+import { UserModule } from './user/user.module';
+import { LocalStrategy } from './auth/local.strategy';
 
 @Module({
-  imports: [StadiumModule, AuthModule, AdminModule, ConfigurationModule],
+  imports: [StadiumModule, AuthModule, AdminModule, ConfigurationModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
